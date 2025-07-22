782726aacc883d9bdd8777c984f7756ef7c36505
Wayne Graham <wgraham@clir.org>
2025-07-22 15:39:41 -0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,njk}',
+    //"./node_modules/tw-elements/js/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        
+      },
+    },
+  },
+  //plugins: [ require("tw-elements/plugin.cjs") ],
+  darkMode: 'class',
+};
